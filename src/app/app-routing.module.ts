cd979e273b8b1db5337d186b45fa3f83ca45f2b5
Ahmed Helmy <42387111+AhmddHelmy@users.noreply.github.com>
2023-10-28 14:45:22 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LayoutComponent } from './home/layout/layout.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
 import { OrgRegitserComponent } from './auth/org-regitser/org-regitser.component';
 import { AdminRegisterComponent } from './auth/admin-register/admin-register.component';
+import { EmpLoginComponent } from './auth/emp-login/emp-login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: LayoutComponent },
   { path: 'admin-register', component: AdminRegisterComponent},
   { path: 'org-register', component: OrgRegitserComponent},
+  { path: 'emp-login', component: EmpLoginComponent},
   { path: '**', component: NotfoundComponent }
 ];
 
