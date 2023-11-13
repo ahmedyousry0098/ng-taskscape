92dd46e099b04189bd1f1b9f0663c29f98de6c77
Ahmed Helmy <42387111+AhmddHelmy@users.noreply.github.com>
2023-11-13 20:38:22 +0200

    update ui

--- src/app/auth/admin-register/admin-register.component.ts
+++ src/app/auth/admin-register/admin-register.component.ts
@@ -32,17 +32,13 @@ export class AdminRegisterComponent {
     this.getOrg();
   }
 
-  registerError() {
-    this.toasterService.error("You havn't entered data correctly");
-  }
-
   isLoading: boolean = false;
   adminRegisterForm: FormGroup = new FormGroup({
     adminName: new FormControl(null, [
       Validators.required,
       personNameValidator(),
     ]),
-    email: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [Validators.required, passwordValidator()]),
   });
 
